a5e4c99dce96eefdd657ec63378d65bbdac1c82e
suely <suelyribeirocm@gmail.com>
2019-09-27 21:44:52 -0300

    criando formulário

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Sistema Web</title>
+</head>
+<body> 
+    <div class="container">
+        <h2>Cadastro de Cliente</h2>
+        <form action="" style="width:500px;">
+            <label for="">Nome:</label>
+            <input type="text" class="form-control">
+            <label for="">Idade:</label>
+            <input type="number" class="form-control">
+            <label for="">E-mail:</label>
+            <input type="email" class="form-control">
+            <br>
+            <input type="submit" class="btn btn-primary">
+        </form>
+    </div>
+</body>
+</html>
